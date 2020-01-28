ce32c2c9014b30547e194b2d63fe550ff2bf2b46
dang lu <danglu@dangs-mbp.lan>
2020-01-27 16:39:13 -0800

    revised code, fixed middleware for jokes router

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,8 +3,20 @@
   before granting access to the next middleware/route handler
 */
 
-module.exports = () => {
-  return (req, res, next) =>{
+// module.exports = () => {
+//   return (req, res, next) =>{
+    
+//     if(!req.session || !req.session.user){
+//       return res.status(401).json({
+//         you: 'shall not pass!'
+//       });
+//     }
+//     next()
+//   }
+
+
+module.exports = (req,res,next) => {
+  try{
     if(!req.session || !req.session.user){
       return res.status(401).json({
         you: 'shall not pass!'
@@ -12,4 +24,8 @@ module.exports = () => {
     }
     next()
   }
-};
+  catch{
+    console.log('error')
+  }
+}
+
